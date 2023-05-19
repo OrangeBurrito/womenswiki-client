55d130f2cae78550cca0cc50dcd1ac005449f3f2
OrangeBurrito <root@orangeburrito.com>
2023-05-19 19:01:40 +0700

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<header>
+    <h2>WomensWiki</h2>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/wiki">Articles</a>
+    </nav>
+</header>
+
+<slot/>
---
+++ src/routes/wiki/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { PageData } from './$types'
+
+    export let data: PageData
+
+    console.log(data)
+</script>
+
+<h1>Articles</h1>
+
+{#each data.json as article}
+    <a class="article" href={`/wiki/${article.slug}`}>
+        <h2>{article.title}</h2>
+    </a>
+{/each}
+
+<style>
+    .article {
+        display: block;
+        padding: 1rem;
+    }
+</style>
