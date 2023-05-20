ef1a84fdc64df59c64e8817b6f6514135449b915
OrangeBurrito <root@orangeburrito.com>
2023-05-20 16:08:38 +0700

    fix: clicking search results don't reload page

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -14,12 +14,30 @@
 	)
 </script>
 
-<input type="text" placeholder="Search for articles" on:input={debounce(search,100)} />
+<input type="text" on:input={debounce(search,100)} bind:value={searchQuery} placeholder="Search for articles" />
 
 {#if searchQuery.length > 0}
-	{#each filteredItems as item}
-		<div class="item">
-			<a href="/wiki/{item.slug}">{item.title}</a>
-		</div>
-	{/each}
+	<div class="search-results">
+		{#each filteredItems as item}
+		<a class="article" href={`/wiki/${item.slug}`} on:click={()=>searchQuery=''} target="_self">{item.title}</a>
+		{/each}
+	</div>
 {/if}
+
+<style>
+	.search-results {
+		width: fit-content;
+		border: 1px solid #333;
+	}
+
+	input, .article {
+		display: block;
+		padding: 0.5rem 1rem;
+	}
+	
+	.article:hover {
+		cursor: pointer;
+		text-decoration: underline;
+		background: #eee;
+	}
+</style>
